93dd6b1e1a8915802b5130cbf6dfd8871e12b743
ChoiSangIl <wo566@naver.com>
2022-01-12 10:23:11 +0900

    jpa query 실행시점

--- src/main/java/jpa/study/JpaStudy.java
+++ src/main/java/jpa/study/JpaStudy.java
@@ -11,40 +11,48 @@ import jpa.study.entity.Team;
 
 public class JpaStudy {
 
-	 public static void main(String[] args) {
-	        //엔티티 매니저 팩토리 생성
-	        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
-	        EntityManager em = emf.createEntityManager(); //엔티티 매니저 생성
-
-	        EntityTransaction tx = em.getTransaction(); //트랜잭션 기능 획득
-
-	        try {
-	            tx.begin(); //트랜잭션 시작
-	            
-	            Team team1 = new Team("team1", "전자상거래");
-	            em.persist(team1);
-	            
-	            Member member1 = new Member("member1", "최상일");
-	            Member member2 = new Member("member2", "김예지");
-	            member1.setTeam(team1);
-	            
-	            System.out.println("1");
-	            em.persist(member1);
-	            System.out.println("2");
-	            em.persist(member2);
-	            System.out.println("3");
-	            em.remove(member1);
-	            System.out.println("4");
-	            em.remove(team1);
-	            System.out.println("5");
-	            
-	            tx.commit();//트랜잭션 커밋
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	            tx.rollback(); //트랜잭션 롤백
-	        } finally {
-	            em.close(); //엔티티 매니저 종료
-	        }
-	        emf.close(); //엔티티 매니저 팩토리 종료
-    }
+	public static void main(String[] args) {
+		//엔티티 매니저 팩토리 생성
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+		EntityManager em = emf.createEntityManager(); //엔티티 매니저 생성
+		
+		EntityTransaction tx = em.getTransaction(); //트랜잭션 기능 획득
+		
+		try {
+		    tx.begin(); //트랜잭션 시작
+		    jpaQueryTiming(em);
+		tx.commit();//트랜잭션 커밋
+		} catch (Exception e) {
+		    e.printStackTrace();
+		    tx.rollback(); //트랜잭션 롤백
+		} finally {
+		    em.close(); //엔티티 매니저 종료
+		}
+		
+		emf.close(); //엔티티 매니저 팩토리 종료	
+	}
+	 
+	 /**
+	 * jap에서 쿼리는 언제 실행 될까?
+	 */
+	private static void jpaQueryTiming(EntityManager em) {
+		Team team1 = new Team("team1", "전자상거래");
+		em.persist(team1);
+		 
+		Member member1 = new Member("member1", "최상일");
+		Member member2 = new Member("member2", "김예지");
+		member1.setTeam(team1);
+		 
+		System.out.println("-1-");
+		em.persist(member1);
+		System.out.println("-2-");
+		em.persist(member2);
+		System.out.println("-3-");
+		em.remove(member1);
+		System.out.println("-4-");
+		em.remove(member2);
+		System.out.println("-5-");
+		em.remove(team1);
+		System.out.println("-6-");
+	}
 }
